6aa9c9bbd1e6bad2c17d97037702ca1dc6e1c5e4
JrHero14 <overload.emperor14@gmail.com>
2022-03-27 20:15:25 +0800

    Change database

--- database/connection.go
+++ database/connection.go
@@ -348,7 +348,7 @@ func ConnectDB() {
 	}
 
 	// Connection URL to connect to Postgres Database
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", config.Config("DB_HOST"), port, config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"))
 	// Connect to the DB and initialize the DB variable
 	DB, err = gorm.Open(postgres.Open(dsn))
 
