4dc8af92e56dfdb51e42fcf6563f6647d3ad9c64
JrHero14 <overload.emperor14@gmail.com>
2022-03-27 22:04:49 +0800

    Fix PORT 2

--- main.go
+++ main.go
@@ -20,9 +20,9 @@ func main() {
 	// Listen on PORT 3000
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8000"
 	}
-	errListen := app.Listen(":8080")
+	errListen := app.Listen(":" + port)
 	if errListen != nil {
 		os.Exit(1)
 	}
