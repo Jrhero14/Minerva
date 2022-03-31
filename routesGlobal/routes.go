93772edd9ea7f4e037f6267d381650e1fe13b5c2
Jrhero14 <Overload.emperor14@gmail.com>
2022-03-31 11:32:52 +0800

    CORS fix

--- routesGlobal/routes.go
+++ routesGlobal/routes.go
@@ -4,11 +4,22 @@ import (
 	adminRoutes "github.com/Jrhero14/Minerva/AdminApp/Routes"
 	authRoutes "github.com/Jrhero14/Minerva/AuthApp/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	// Setup the Node Routes
+	// Or extend your config for customization
+	api.Use(cors.New(cors.Config{
+		Next:             nil,
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders:     "",
+		AllowCredentials: false,
+		ExposeHeaders:    "",
+		MaxAge:           0,
+	}))
 	authRoutes.SetupNoteRoutes(api)
 	adminRoutes.SetupNoteRoutes(api)
 }
